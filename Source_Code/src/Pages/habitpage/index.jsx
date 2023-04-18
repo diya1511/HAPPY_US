0be04db7cfc2e35da4d79a4a8d028a0dff9c24b2
0-Parth-D <89337450+0-Parth-D@users.noreply.github.com>
2023-04-18 19:00:57 +0530

    Updated Habit Page

--- Source_Code/src/Pages/habitpage/index.jsx
+++ Source_Code/src/Pages/habitpage/index.jsx
@@ -1,27 +1,64 @@
-import './styles.css';
-import Navbar from '../../Components/Navbar';
-import HabitCounter from '../../Components/Habit Counter';
-import FriendListHome from '../../Components/Friend Box Home';
-import HabitCalendar from '../../Components/Habit Calender';
-const Habitpage = () => {
-  return (
-    <div className="Background">
-      <div className="contents">
-        <Navbar />
-        <div className="habit-tracker">
-          <HabitCalendar />
-          <div className="habit-counter">
-            <HabitCounter />
-            <HabitCounter />
-            <HabitCounter />
-            <HabitCounter />
-            <HabitCounter />
+import { useState } from "react";
+import "./styles.css";
+import Navbar from "../../Components/Navbar";
+import HabitCounter from "../../Components/Habit Counter";
+import FreindListHome from "../../Components/Friend Box Home";
+import Calendar from "../../Components/Habit Calender";
+
+const Habitpage = (props) => {
+
+    const [numComponents, setNumComponents] = useState(0);
+
+    const handleClick = () => {
+      setNumComponents(numComponents + 1);
+    };
+  
+    const components = [];
+    for (let i = 0; i < numComponents; i++) {
+      components.push(<HabitCounter key={i} />);
+    }
+
+    return (
+      <div className="Background">
+        <div className="contents">
+          <div className="Navbar">
+            <Navbar />
+          </div>
+          <div className="add-btn-container">
+            <button onClick={handleClick} className="send-parent">
+                <div className="send">
+                    + Add a Habit
+                </div>
+            </button>
+          </div>
+          <div className="habit-tracker">
+            <p className="title-habit">Habit Tracker</p>
+            <div class="line-container">
+              <svg class="line-svg">
+                <line
+                  x1="0"
+                  y1="0"
+                  x2="100%"
+                  y2="0"
+                  stroke="#eee"
+                  stroke-width="1"
+                />
+              </svg>
+            </div>
+
+            <div className="calender">
+              <Calendar />
+            </div>
+            <div className="habit-counter">
+                {components}
+            </div>
+          </div>
+          <div className="tools-habit">
+            <FreindListHome />
           </div>
         </div>
-        <FriendListHome />
       </div>
-    </div>
-  );
-};
+    );
+}
 
 export default Habitpage;
