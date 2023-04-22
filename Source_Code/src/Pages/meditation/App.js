457017fb02fa48e57b136c57dbfc7ac35d4a8008
harshak2001 <harshak03.62@gmail.com>
2023-04-22 14:49:54 +0530

    harshak

--- Source_Code/src/Pages/meditation/App.js
+++ Source_Code/src/Pages/meditation/App.js
@@ -2,19 +2,11 @@ import React from "react";
 import { useState } from "react";
 // import MeditationDetail from "./components/MeditationDetail";
 import MedStart from "./components/MedStart";
-import './components/MedStart.css'
-import Meditation from './components/Meditation'
-import {Route,Routes } from "react-router-dom";
-
 
 const App = () => {
   return (
     <div>
       <MedStart></MedStart>
-
-      <Routes>
-        <Route path='/meditation' element={<Meditation></Meditation>}></Route>
-      </Routes>
     </div>
   )
 }
