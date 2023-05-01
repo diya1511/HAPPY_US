78e7f1d2529e2b8e2ed394ab134aee96e0cac299
Diya Agrawal <diya.22010358@viit.ac.in>
2023-05-01 19:35:20 +0530

    some pics

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const cors = require('cors');
+const path = require('path');
+// import { fileURLToPath } from "url";
 const morgan = require('morgan');
 const multer = require('multer');
 const postController = require('./controllers/postController');
 const app = express();
 const userModel = require('./models/userModel');
-const postModel = require('./models/postModel');
+const postModel = require('./models/userModel.js');
 const { users, posts } = require('./data/index');
 dotenv.config();
 
@@ -21,13 +24,20 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// storage
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null)
-//   }
-// });
-const upload = multer({ des: 'public/img/users' });
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 app.post('/posts', upload.single('picture'), postController.createPost);
 // routes
 app.use('/api/v1/auth', authRoutes);
@@ -36,7 +46,5 @@ app.use('/api/v1/posts', postRoutes);
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
-  //Add Data one time
-  userModel.insertMany(users);
-  postModel.insertMany(posts);
+ 
 });
